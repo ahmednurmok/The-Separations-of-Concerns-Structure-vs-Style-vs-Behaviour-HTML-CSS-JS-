e124e4fbcf0ef95583297fac3c0f5223c524ce81
ahmednurmok <ahmednurmok@gmail.com>
2025-11-09 23:03:28 +0000

    Add files via upload

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+
+//Task: Query and create and remove CSS classes using JavaScript
+
+document.querySelector('button').classlist
+document.querySelector('button').classlist.add('invisible')
+document.querySelector('button').classlist.remove('invisible')
+
+
